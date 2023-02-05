c0f1bce6e62123a03267cb35686b11b1427ed58c
19church <saint2737@gmail.com>
2023-02-05 20:50:04 +0700

    testdata

---
+++ testdata.py
@@ -0,0 +1,99 @@
+data1 = {
+    "Dormitory_Name":"ข้าวโพด",
+    "Room_Number":"001",
+    "Room_Size":"3.00  meter square",
+    "Monthly_Rent":"1 BTC",
+    "Current_Roomer":"ลักยิ้ม",
+    "District":"ชาไทย",
+    "Province":"หมูทะ"
+}
+
+data2 = {
+    "Dormitory_Name":"ข้าวโพด",
+    "Room_Number":"002",
+    "Room_Size":"3.00  meter square",
+    "Monthly_Rent":"1 BTC",
+    "Current_Roomer":"หมวกกันน็อค",
+    "District":"ชาไทย",
+    "Province":"หมูทะ"
+}
+
+data3 = {
+    "Dormitory_Name":"ข้าวโพด",
+    "Room_Number":"003",
+    "Room_Size":"3.00  meter square",
+    "Monthly_Rent":"1 BTC",
+    "Current_Roomer":"โล้นผ้าคลุม",
+    "District":"ชาไทย",
+    "Province":"หมูทะ"
+}
+
+data4 = {
+    "Dormitory_Name":"ข้าวโพด",
+    "Room_Number":"004",
+    "Room_Size":"3.00  meter square",
+    "Monthly_Rent":"1 BTC",
+    "Current_Roomer":"อสูรไซบอร์ก",
+    "District":"ชาไทย",
+    "Province":"หมูทะ"
+}
+
+data5 = {
+    "Dormitory_Name":"ข้าวโพด",
+    "Room_Number":"005",
+    "Room_Size":"3.00  meter square",
+    "Monthly_Rent":"1 BTC",
+    "Current_Roomer":"แฟลช ประกายแสง",
+    "District":"ชาไทย",
+    "Province":"หมูทะ"
+}
+
+data6 = {
+    "Dormitory_Name":"ข้าวโพด",
+    "Room_Number":"006",
+    "Room_Size":"3.00  meter square",
+    "Monthly_Rent":"1 BTC",
+    "Current_Roomer":"แทงค์ท็อปมาสเตอร์",
+    "District":"ชาไทย",
+    "Province":"หมูทะ"
+}
+
+data7 = {
+    "Dormitory_Name":"ข้าวโพด",
+    "Room_Number":"007",
+    "Room_Size":"3.00  meter square",
+    "Monthly_Rent":"1 BTC",
+    "Current_Roomer":"ไรเดอร์ไร้ใบขับขี่",
+    "District":"ชาไทย",
+    "Province":"หมูทะ"
+}
+
+data8 = {
+    "Dormitory_Name":"ข้าวโพด",
+    "Room_Number":"008",
+    "Room_Size":"3.00  meter square",
+    "Monthly_Rent":"1 BTC",
+    "Current_Roomer":"โซนิค ความเร็วเสียง",
+    "District":"ชาไทย",
+    "Province":"หมูทะ"
+}
+
+data9 = {
+    "Dormitory_Name":"ข้าวโพด",
+    "Room_Number":"009",
+    "Room_Size":"3.00  meter square",
+    "Monthly_Rent":"1 BTC",
+    "Current_Roomer":"เรเก็น พลังวิญญาณ",
+    "District":"ชาไทย",
+    "Province":"หมูทะ"
+}
+
+data10 = {
+    "Dormitory_Name":"ข้าวโพด",
+    "Room_Number":"010",
+    "Room_Size":"3.00  meter square",
+    "Monthly_Rent":"1 BTC",
+    "Current_Roomer":"อสูรเท็นกะ",
+    "District":"ชาไทย",
+    "Province":"หมูทะ"
+}
